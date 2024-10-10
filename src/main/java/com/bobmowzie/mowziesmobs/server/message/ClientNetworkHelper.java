8c3b5fb037a734e603f872341d7d0dd635644a0f
IAFEnvoy <tanksheng545@126.com>
2024-10-10 15:44:34 +0800

    fix #2

--- src/main/java/com/bobmowzie/mowziesmobs/server/message/ClientNetworkHelper.java
+++ src/main/java/com/bobmowzie/mowziesmobs/server/message/ClientNetworkHelper.java
@@ -12,7 +12,6 @@ import com.bobmowzie.mowziesmobs.server.entity.ILinkedEntity;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
@@ -25,26 +24,29 @@ public class ClientNetworkHelper {
     public static void register() {
         ClientPlayNetworking.registerGlobalReceiver(StaticVariables.BLACK_PINK_IN_YOUR_AREA, (client, handler, buf, responseSender) -> {
             MessageBlackPinkInYourArea message = MessageBlackPinkInYourArea.deserialize(buf);
-            ClientWorld world = MinecraftClient.getInstance().world;
-            assert world != null;
-            Entity entity = world.getEntityById(message.getEntityID());
-            if (entity instanceof AbstractMinecartEntity minecart) {
-                MowziesMobs.PROXY.playBlackPinkSound(minecart);
-                BlockState state = Blocks.STONE.getDefaultState().with(BlockGrottol.VARIANT, BlockGrottol.Variant.BLACK_PINK);
-                BlockPos pos = minecart.getBlockPos();
-                final float scale = 0.75F;
-                double x = minecart.getX();
-                double y = minecart.getY() + 0.375F + 0.5F + (minecart.getDefaultBlockOffset() - 8) / 16.0F * scale;
-                double z = minecart.getZ();
-                BlockSoundGroup sound = state.getBlock().getSoundGroup(state);
-                world.playSound(x, y, z, sound.getBreakSound(), minecart.getSoundCategory(), (sound.getVolume() + 1.0F) / 2.0F, sound.getPitch() * 0.8F, false);
-                MowziesMobs.PROXY.minecartParticles(world, minecart, scale, x, y, z, state, pos);
-            }
+            client.execute(() -> {
+                ClientWorld world = client.world;
+                assert world != null;
+                Entity entity = world.getEntityById(message.getEntityID());
+                if (entity instanceof AbstractMinecartEntity minecart) {
+                    MowziesMobs.PROXY.playBlackPinkSound(minecart);
+                    BlockState state = Blocks.STONE.getDefaultState().with(BlockGrottol.VARIANT, BlockGrottol.Variant.BLACK_PINK);
+                    BlockPos pos = minecart.getBlockPos();
+                    final float scale = 0.75F;
+                    double x = minecart.getX();
+                    double y = minecart.getY() + 0.375F + 0.5F + (minecart.getDefaultBlockOffset() - 8) / 16.0F * scale;
+                    double z = minecart.getZ();
+                    BlockSoundGroup sound = state.getBlock().getSoundGroup(state);
+                    world.playSound(x, y, z, sound.getBreakSound(), minecart.getSoundCategory(), (sound.getVolume() + 1.0F) / 2.0F, sound.getPitch() * 0.8F, false);
+                    MowziesMobs.PROXY.minecartParticles(world, minecart, scale, x, y, z, state, pos);
+                }
+            });
         });
         ClientPlayNetworking.registerGlobalReceiver(StaticVariables.FREEZE_EFFECT, (client, handler, buf, responseSender) -> {
             MessageFreezeEffect message = MessageFreezeEffect.deserialize(buf);
-            if (MinecraftClient.getInstance().world != null) {
-                Entity entity = MinecraftClient.getInstance().world.getEntityById(message.getEntityID());
+            client.execute(() -> {
+                assert client.world != null;
+                Entity entity = client.world.getEntityById(message.getEntityID());
                 if (entity instanceof LivingEntity living) {
                     FrozenCapability.IFrozenCapability livingCapability = FrozenCapability.get(living);
                     if (livingCapability != null) {
@@ -52,70 +54,80 @@ public class ClientNetworkHelper {
                         else livingCapability.onUnfreeze(living);
                     }
                 }
-            }
+            });
         });
         ClientPlayNetworking.registerGlobalReceiver(StaticVariables.INTERRUPT_ABILITY, (client, handler, buf, responseSender) -> {
             MessageInterruptAbility message = MessageInterruptAbility.deserialize(buf);
-            LivingEntity entity = (LivingEntity) MinecraftClient.getInstance().world.getEntityById(message.getEntityID());
-            if (entity != null) {
-                AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(entity);
-                if (abilityCapability != null) {
-                    AbilityType<?, ?> abilityType = abilityCapability.getAbilityTypesOnEntity(entity)[message.getIndex()];
-                    Ability<?> instance = abilityCapability.getAbilityMap().get(abilityType);
-                    if (instance.isUsing()) instance.interrupt();
+            client.execute(() -> {
+                assert client.world != null;
+                LivingEntity entity = (LivingEntity) client.world.getEntityById(message.getEntityID());
+                if (entity != null) {
+                    AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(entity);
+                    if (abilityCapability != null) {
+                        AbilityType<?, ?> abilityType = abilityCapability.getAbilityTypesOnEntity(entity)[message.getIndex()];
+                        Ability<?> instance = abilityCapability.getAbilityMap().get(abilityType);
+                        if (instance != null && instance.isUsing()) instance.interrupt();
+                    }
                 }
-            }
+            });
         });
         ClientPlayNetworking.registerGlobalReceiver(StaticVariables.JUMP_TO_ABILITY_SECTION, (client, handler, buf, responseSender) -> {
             MessageJumpToAbilitySection message = MessageJumpToAbilitySection.deserialize(buf);
-            LivingEntity entity = (LivingEntity) MinecraftClient.getInstance().world.getEntityById(message.getEntityID());
-            if (entity != null) {
-                AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(entity);
-                if (abilityCapability != null) {
-                    AbilityType<?, ?> abilityType = abilityCapability.getAbilityTypesOnEntity(entity)[message.getIndex()];
-                    Ability<?> instance = abilityCapability.getAbilityMap().get(abilityType);
-                    if (instance.isUsing()) instance.jumpToSection(message.getSectionIndex());
+            client.execute(() -> {
+                assert client.world != null;
+                LivingEntity entity = (LivingEntity) client.world.getEntityById(message.getEntityID());
+                if (entity != null) {
+                    AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(entity);
+                    if (abilityCapability != null) {
+                        AbilityType<?, ?> abilityType = abilityCapability.getAbilityTypesOnEntity(entity)[message.getIndex()];
+                        Ability<?> instance = abilityCapability.getAbilityMap().get(abilityType);
+                        if (instance.isUsing()) instance.jumpToSection(message.getSectionIndex());
+                    }
                 }
-            }
+            });
         });
         ClientPlayNetworking.registerGlobalReceiver(StaticVariables.LINK_ENTITIES, (client, handler, buf, responseSender) -> {
             MessageLinkEntities message = MessageLinkEntities.deserialize(buf);
-            World world = MinecraftClient.getInstance().world;
-            if (world != null) {
+            client.execute(() -> {
+                World world = client.world;
+                assert world != null;
                 Entity entitySource = world.getEntityById(message.getSourceID());
                 Entity entityTarget = world.getEntityById(message.getTargetID());
                 if (entitySource instanceof ILinkedEntity linked && entityTarget != null)
                     linked.link(entityTarget);
-            }
+            });
         });
         ClientPlayNetworking.registerGlobalReceiver(StaticVariables.SUNBLOCK_EFFECT, (client, handler, buf, responseSender) -> {
             MessageSunblockEffect message = MessageSunblockEffect.deserialize(buf);
-            if (MinecraftClient.getInstance().world != null) {
-                Entity entity = MinecraftClient.getInstance().world.getEntityById(message.getEntityID());
+            client.execute(() -> {
+                assert client.world != null;
+                Entity entity = client.world.getEntityById(message.getEntityID());
                 if (entity instanceof LivingEntity living) {
                     LivingCapability.ILivingCapability livingCapability = LivingCapability.get(living);
                     if (livingCapability != null)
                         livingCapability.setHasSunblock(message.hasSunblock());
                 }
-            }
+            });
         });
 
         ClientPlayNetworking.registerGlobalReceiver(StaticVariables.UPDATE_BOSS_BAR, (client, handler, buf, responseSender) -> {
             MessageUpdateBossBar message = MessageUpdateBossBar.deserialize(buf);
-            if (message.getRegistryName() == null) {
-                ClientProxy.bossBarRegistryNames.remove(message.getBossID());
-            } else {
-                ClientProxy.bossBarRegistryNames.put(message.getBossID(), message.getRegistryName());
-            }
+            client.execute(() -> {
+                if (message.getRegistryName() == null) ClientProxy.bossBarRegistryNames.remove(message.getBossID());
+                else ClientProxy.bossBarRegistryNames.put(message.getBossID(), message.getRegistryName());
+            });
         });
         ClientPlayNetworking.registerGlobalReceiver(StaticVariables.USE_ABILITY, (client, handler, buf, responseSender) -> {
             MessageUseAbility message = MessageUseAbility.deserialize(buf);
-            LivingEntity entity = (LivingEntity) MinecraftClient.getInstance().world.getEntityById(message.getEntityID());
-            if (entity != null) {
-                AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(entity);
-                if (abilityCapability != null)
-                    abilityCapability.activateAbility(entity, abilityCapability.getAbilityTypesOnEntity(entity)[message.getIndex()]);
-            }
+            client.execute(() -> {
+                assert client.world != null;
+                LivingEntity entity = (LivingEntity) client.world.getEntityById(message.getEntityID());
+                if (entity != null) {
+                    AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(entity);
+                    if (abilityCapability != null)
+                        abilityCapability.activateAbility(entity, abilityCapability.getAbilityTypesOnEntity(entity)[message.getIndex()]);
+                }
+            });
         });
     }
 }
--- src/main/java/com/bobmowzie/mowziesmobs/server/message/ServerNetworkHelper.java
+++ src/main/java/com/bobmowzie/mowziesmobs/server/message/ServerNetworkHelper.java
@@ -23,79 +23,71 @@ import net.minecraft.screen.ScreenHandler;
 
 public class ServerNetworkHelper {
     public static void register() {
-        ServerPlayNetworking.registerGlobalReceiver(StaticVariables.LEFT_MOUSE_DOWN, (server, player, handler, buf, responseSender) -> {
-            if (player != null) {
-                PlayerCapability.IPlayerCapability capability = PlayerCapability.get(player);
-                if (capability != null) {
-                    capability.setMouseLeftDown(true);
-                    Power[] powers = capability.getPowers();
-                    for (Power power : powers)
-                        power.onLeftMouseDown(player);
-                }
-                AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(player);
-                if (abilityCapability != null)
-                    for (Ability<?> ability : abilityCapability.getAbilities())
-                        if (ability instanceof PlayerAbility playerAbility)
-                            playerAbility.onLeftMouseDown(player);
+        ServerPlayNetworking.registerGlobalReceiver(StaticVariables.LEFT_MOUSE_DOWN, (server, player, handler, buf, responseSender) -> server.execute(() -> {
+            PlayerCapability.IPlayerCapability capability = PlayerCapability.get(player);
+            if (capability != null) {
+                capability.setMouseLeftDown(true);
+                Power[] powers = capability.getPowers();
+                for (Power power : powers)
+                    power.onLeftMouseDown(player);
             }
-        });
-        ServerPlayNetworking.registerGlobalReceiver(StaticVariables.LEFT_MOUSE_UP, (server, player, handler, buf, responseSender) -> {
-            if (player != null) {
-                PlayerCapability.IPlayerCapability capability = PlayerCapability.get(player);
-                if (capability != null) {
-                    capability.setMouseLeftDown(false);
-                    Power[] powers = capability.getPowers();
-                    for (Power power : powers)
-                        power.onLeftMouseUp(player);
-                }
-                AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(player);
-                if (abilityCapability != null)
-                    for (Ability<?> ability : abilityCapability.getAbilities())
-                        if (ability instanceof PlayerAbility playerAbility)
-                            playerAbility.onLeftMouseUp(player);
+            AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(player);
+            if (abilityCapability != null)
+                for (Ability<?> ability : abilityCapability.getAbilities())
+                    if (ability instanceof PlayerAbility playerAbility)
+                        playerAbility.onLeftMouseDown(player);
+        }));
+        ServerPlayNetworking.registerGlobalReceiver(StaticVariables.LEFT_MOUSE_UP, (server, player, handler, buf, responseSender) -> server.execute(() -> {
+            PlayerCapability.IPlayerCapability capability = PlayerCapability.get(player);
+            if (capability != null) {
+                capability.setMouseLeftDown(false);
+                Power[] powers = capability.getPowers();
+                for (Power power : powers)
+                    power.onLeftMouseUp(player);
             }
-        });
-        ServerPlayNetworking.registerGlobalReceiver(StaticVariables.RIGHT_MOUSE_DOWN, (server, player, handler, buf, responseSender) -> {
-            if (player != null) {
-                PlayerCapability.IPlayerCapability capability = PlayerCapability.get(player);
-                if (capability != null) {
-                    capability.setMouseRightDown(true);
-                    Power[] powers = capability.getPowers();
-                    for (Power power : powers)
-                        power.onRightMouseDown(player);
-                }
-
-                AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(player);
-                if (abilityCapability != null)
-                    for (Ability<?> ability : abilityCapability.getAbilities())
-                        if (ability instanceof PlayerAbility playerAbility)
-                            playerAbility.onRightMouseDown(player);
+            AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(player);
+            if (abilityCapability != null)
+                for (Ability<?> ability : abilityCapability.getAbilities())
+                    if (ability instanceof PlayerAbility playerAbility)
+                        playerAbility.onLeftMouseUp(player);
+        }));
+        ServerPlayNetworking.registerGlobalReceiver(StaticVariables.RIGHT_MOUSE_DOWN, (server, player, handler, buf, responseSender) -> server.execute(() -> {
+            PlayerCapability.IPlayerCapability capability = PlayerCapability.get(player);
+            if (capability != null) {
+                capability.setMouseRightDown(true);
+                Power[] powers = capability.getPowers();
+                for (Power power : powers)
+                    power.onRightMouseDown(player);
             }
-        });
-        ServerPlayNetworking.registerGlobalReceiver(StaticVariables.RIGHT_MOUSE_UP, (server, player, handler, buf, responseSender) -> {
-            if (player != null) {
-                PlayerCapability.IPlayerCapability capability = PlayerCapability.get(player);
-                if (capability != null) {
-                    capability.setMouseRightDown(false);
-                    Power[] powers = capability.getPowers();
-                    for (Power power : powers)
-                        power.onRightMouseUp(player);
-                }
-                AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(player);
-                if (abilityCapability != null)
-                    for (Ability<?> ability : abilityCapability.getAbilities())
-                        if (ability instanceof PlayerAbility playerAbility)
-                            playerAbility.onRightMouseUp(player);
+            AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(player);
+            if (abilityCapability != null)
+                for (Ability<?> ability : abilityCapability.getAbilities())
+                    if (ability instanceof PlayerAbility playerAbility)
+                        playerAbility.onRightMouseDown(player);
+        }));
+        ServerPlayNetworking.registerGlobalReceiver(StaticVariables.RIGHT_MOUSE_UP, (server, player, handler, buf, responseSender) -> server.execute(() -> {
+            PlayerCapability.IPlayerCapability capability = PlayerCapability.get(player);
+            if (capability != null) {
+                capability.setMouseRightDown(false);
+                Power[] powers = capability.getPowers();
+                for (Power power : powers)
+                    power.onRightMouseUp(player);
             }
-        });
+            AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(player);
+            if (abilityCapability != null)
+                for (Ability<?> ability : abilityCapability.getAbilities())
+                    if (ability instanceof PlayerAbility playerAbility)
+                        playerAbility.onRightMouseUp(player);
+        }));
         ServerPlayNetworking.registerGlobalReceiver(StaticVariables.PLAYER_ATTACK_MOB, (server, player, handler, buf, responseSender) -> {
             MessagePlayerAttackMob message = MessagePlayerAttackMob.deserialize(buf);
-            if (player != null) {
+            server.execute(() -> {
                 Entity entity = player.getWorld().getEntityById(message.getEntityID());
                 if (entity != null) player.attack(entity);
-            }
+            });
         });
-        ServerPlayNetworking.registerGlobalReceiver(StaticVariables.PLAYER_SOLAR_BEAM, (server, player, handler, buf, responseSender) -> {
+        ServerPlayNetworking.registerGlobalReceiver(StaticVariables.PLAYER_SOLAR_BEAM, (server, player, handler, buf, responseSender) -> server.execute(() -> {
+            if (!player.hasStatusEffect(EffectHandler.SUNS_BLESSING)) return;
             EntitySolarBeam solarBeam = new EntitySolarBeam(EntityHandler.SOLAR_BEAM, player.getWorld(), player, player.getX(), player.getY() + 1.2f, player.getZ(), (float) ((player.headYaw + 90) * Math.PI / 180), (float) (-player.getPitch() * Math.PI / 180), 55);
             solarBeam.setHasPlayer(true);
             player.getWorld().spawnEntity(solarBeam);
@@ -105,65 +97,56 @@ public class ServerNetworkHelper {
             int solarBeamCost = ConfigHandler.COMMON.TOOLS_AND_ABILITIES.SUNS_BLESSING.solarBeamCost * 60 * 20;
             if (duration - solarBeamCost > 0)
                 player.addStatusEffect(new StatusEffectInstance(EffectHandler.SUNS_BLESSING, duration - solarBeamCost, 0, false, false));
-        });
+        }));
         ServerPlayNetworking.registerGlobalReceiver(StaticVariables.PLAYER_USE_ABILITY, (server, player, handler, buf, responseSender) -> {
             MessagePlayerUseAbility message = MessagePlayerUseAbility.deserialize(buf);
-            AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(player);
-            if (abilityCapability != null) {
-                AbilityHandler.INSTANCE.sendAbilityMessage(player, abilityCapability.getAbilityTypesOnEntity(player)[message.getIndex()]);
-            }
+            server.execute(() -> {
+                AbilityCapability.IAbilityCapability abilityCapability = AbilityCapability.get(player);
+                if (abilityCapability != null)
+                    AbilityHandler.INSTANCE.sendAbilityMessage(player, abilityCapability.getAbilityTypesOnEntity(player)[message.getIndex()]);
+            });
         });
         ServerPlayNetworking.registerGlobalReceiver(StaticVariables.SCULPTOR_TRADE, (server, player, handler, buf, responseSender) -> {
             MessageSculptorTrade message = MessageSculptorTrade.deserialize(buf);
-            if (player != null) {
+            server.execute(() -> {
                 Entity entity = player.getWorld().getEntityById(message.getEntityID());
-                if (!(entity instanceof EntitySculptor sculptor))
-                    return;
-                if (sculptor.getCustomer() != player)
-                    return;
+                if (!(entity instanceof EntitySculptor sculptor)) return;
+                if (sculptor.getCustomer() != player) return;
                 ScreenHandler container = player.currentScreenHandler;
-                if (!(container instanceof ContainerSculptorTrade sculptorTrade))
-                    return;
-                if (sculptor.checkTestObstructed())
-                    return;
+                if (!(container instanceof ContainerSculptorTrade sculptorTrade)) return;
+                if (sculptor.checkTestObstructed()) return;
                 boolean satisfied = sculptor.fulfillDesire(container.getSlot(0));
                 if (satisfied) {
                     sculptorTrade.returnItems();
                     container.sendContentUpdates();
                     sculptor.setTestingPlayer(player);
                 }
-            }
+            });
         });
         ServerPlayNetworking.registerGlobalReceiver(StaticVariables.UMVUTHI_TRADE, (server, player, handler, buf, responseSender) -> {
             MessageUmvuthiTrade message = MessageUmvuthiTrade.deserialize(buf);
-            if (player != null) {
+            server.execute(() -> {
                 Entity entity = player.getWorld().getEntityById(message.getEntityID());
-                if (!(entity instanceof EntityUmvuthi umvuthi)) {
-                    return;
-                }
-                if (umvuthi.getCustomer() != player) {
-                    return;
-                }
+                if (!(entity instanceof EntityUmvuthi umvuthi)) return;
+                if (umvuthi.getCustomer() != player) return;
                 ScreenHandler container = player.currentScreenHandler;
-                if (!(container instanceof ContainerUmvuthiTrade)) {
-                    return;
-                }
+                if (!(container instanceof ContainerUmvuthiTrade)) return;
                 boolean satisfied = umvuthi.hasTradedWith(player);
-                if (!satisfied) {
-                    if (satisfied = umvuthi.fulfillDesire(container.getSlot(0))) {
-                        umvuthi.rememberTrade(player);
-                        ((ContainerUmvuthiTrade) container).returnItems();
-                        container.sendContentUpdates();
-                    }
-                }
                 if (satisfied) {
                     player.addStatusEffect(new StatusEffectInstance(EffectHandler.SUNS_BLESSING, -1, 0, false, false));
                     if (umvuthi.getActiveAbilityType() != EntityUmvuthi.BLESS_ABILITY) {
                         umvuthi.sendAbilityMessage(EntityUmvuthi.BLESS_ABILITY);
                         umvuthi.playSound(MMSounds.ENTITY_UMVUTHI_BLESS, 2, 1);
                     }
+                } else {
+                    satisfied = umvuthi.fulfillDesire(container.getSlot(0));
+                    if (satisfied) {
+                        umvuthi.rememberTrade(player);
+                        ((ContainerUmvuthiTrade) container).returnItems();
+                        container.sendContentUpdates();
+                    }
                 }
-            }
+            });
         });
     }
 }
